269856d951cb1ccf1b105bb2d2f1b4ec8ff4064b
Huzaifa986 <umaisahmed89@gmail.com>
2024-09-28 13:53:18 -0700

    routine

---
+++ script.js
@@ -0,0 +1,16 @@
+  a = prompt ('input time just hour with am or pm');
+/*let a = "6am"
+let b = "7am"t
+let c = "8am"*/
+if (a == "6am" || a == "7am"  || a == "8am" || a == "9am" || a == "10am" || a == "11am"  ) {
+    alert("good morning")
+}
+if ( a ==  "12pm" || a == "1pm" ){
+    alert("good after noon")
+}
+if (a == "2pm" || a == "3pm"  || a == "4pm" || a == "5pm" || a == "6pm" ) {
+    alert("good evening")
+}
+if ( a == "7pm"  || a == "8pm" || a == "9m" || a == "10pm" || a == "11pm" 
+        || a == "12am" || a == "1am" || a == "2amm"  || a == "3am" || a == "4am" || a == "5am" ) {
+    alert("good night")} 
